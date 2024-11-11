9e73402f7ba8b9b655fc22a1e1c14a0c45368ed0
Timmatus <timmatusik@gmail.com>
2024-11-11 22:37:54 +0300

    Initial commit

---
+++ app/src/main/java/com/mgke/lr23/MainActivity.java
@@ -0,0 +1,59 @@
+package com.mgke.lr23;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText emailAddress, subject, message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Найдем элементы по id
+        emailAddress = findViewById(R.id.email_address);
+        subject = findViewById(R.id.subject);
+        message = findViewById(R.id.message);
+        Button sendButton = findViewById(R.id.send_button);
+
+
+
+        // Устанавливаем обработчик нажатия на кнопку
+        sendButton.setOnClickListener(v -> {
+            String email = emailAddress.getText().toString().trim();
+
+            // Проверка на валидность email адреса
+            if (!isValidEmail(email)) {
+                emailAddress.setError("Введите корректный адрес электронной почты");
+                return; // Прекращаем выполнение, если email невалиден
+            }
+
+            // Очищаем текстовые поля
+            emailAddress.setText("");
+            subject.setText("");
+            message.setText("");
+
+
+            // Переходим на новую активность
+            Intent intent = new Intent(MainActivity.this, NextActivity.class);
+            startActivity(intent);
+        });
+    }
+
+
+
+    private boolean isValidEmail(String email) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
+    }
+}
